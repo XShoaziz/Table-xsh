1c040fadbe67f768aa4c43a71ff7361d4a5d1620
XShoaziz <xusanovshoaziz@gmail.com>
2022-09-10 18:46:11 +0500

    salom

---
+++ dars3/dars5/index.js
@@ -0,0 +1,20 @@
+// let str = "salom";
+// for (let i = 0; i < str.length; i++) {
+//   console.log(str[i] + "1");
+// }
+// let str = "salom";
+// for (let i = 0; i < str.length; i++) {
+//   console.log(str[i].toLocaleUpperCase());
+// }
+// let str = "sa32422lo32141m";
+// for (let i = 0; i < str.length; i++) {
+//   if (+str[i]) console.log(str[i]);
+// }
+// let str = "sa32422lo32141m";
+// console.log(str.match(/[0-9]/g).join());
+//////////////////
+let str = "a3b3c4";
+for (let i = 0; i < str.length; i++) {
+  let str2 = "";
+  console.log(str[0].repeat(2));
+}
---
+++ dars3/index.js
@@ -0,0 +1,16 @@
+// let a = "salom";
+// let b = a.replace("salom", "dunyo\n");
+// console.log(b.repeat(10));
+// let a = "hi";
+// let b = "bye";
+// let x = a.concat(b);
+// let d = x.concat(b);
+// console.log(d.repeat(2));
+//////////////////////////////////////////////////
+let a = "hi";
+let b = "bye";
+function name1(a, b) {
+  return a + b + b + a;
+}
+console.log(name1(a, b));
+//////////////////////////////////
